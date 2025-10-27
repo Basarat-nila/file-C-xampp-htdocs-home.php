287b5368ab47c82326af7b008f30b303716f65ad
Basarat Binte Basar <basaratnila@gmail.com>
2025-10-28 03:14:26 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+           <title>My first page</title>
+</head>
+<body>
+       <h1 style="text-align: center; front-size: 1px;'> Basarat Binte Basar</h1>
+       <h2> Introduction:<h2/>
+       <p> This is Basarat Binte Basar. I am currently studying BSC in CSE at AIUB. I live in Dhaka<p/>
+       <h2>Education:<h2/>
+       <h3>BSC<h3/>
+       <p> From AIUB<p/>
+       <h2> Interest:<h2/>
+       <h3>Travelling<h3/>
+       <p> Exploring new places<p/>
+       <h2> Soft Skill:<h2/>
+       <h3>Motivated Student<h3/>
+       <p> TeamWork Skills<p/>
+       <h2> Project:<h2/>
+       <h3>Courier Management System in PHP & MySQL<h3/>
+       <p>developed in PHP and MySQL<p/>
+       <h2> Reference<h2/>
+       <h3>https://projectworlds.com/courier-management-system-in-php-mysql/<h3/>
+       <p>website link<p/>
+</body>
+      <html/>
